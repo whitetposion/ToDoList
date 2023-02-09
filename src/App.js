fa3a0757b2db5baf28977b07fd095fd1dfba18b0
abhishek <purohitabhishek308@gmail.com>
2023-02-09 08:43:49 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React,{Component} from 'react';
 import './App.css';
 import Greet from './components/Greet';
+import { useState } from 'react';
 //import Welcome from './components/Welcome';
 
 function App() {
+  const [age, setAge]= useState(0);
+  const increasedAge=()=>{
+    setAge(age+1);
+    
+  }; 
+  const [inputvalue, setinputvlaue]= useState("")
+  const handleInput = (event) => {
+    setinputvlaue(event.target.value);
+
+  }
+  const [text,settext ]=  useState(true);
   
   const user=[
     {name:"Mars" ,isGasPlanet: false},
@@ -19,8 +31,12 @@ function App() {
         {user.map((nmme,key)=>!nmme.isGasPlanet && <h1 >{nmme.name} </h1>
         )}
         <Greet/>
-        
-        
+        <div>{age} <button onClick={increasedAge}>increase age</button></div>
+        <input type="text" onChange={handleInput}/>{inputvalue}
+        <button 
+        onClick={() => {settext(!text);
+        }}>show/hide</button>
+        {text && <h1>i can disappear</h1>}
       </div>
     );
   
