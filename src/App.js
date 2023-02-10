d8a155f4f7afc4725b1cdfc3aafd3ed45c6ea17d
abhishek <purohitabhishek308@gmail.com>
2023-02-10 14:32:37 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
 import React,{Component} from 'react';
 import './App.css';
-import Greet from './components/Greet';
+
 import { useState } from 'react';
-//import Welcome from './components/Welcome';
+
 
 function App() {
-  const [age, setAge]= useState(0);
-  const increasedAge=()=>{
-    setAge(age+1);
-    
-  }; 
-  const [inputvalue, setinputvlaue]= useState("")
-  const handleInput = (event) => {
-    setinputvlaue(event.target.value);
+  const [newTask, setNewTask] = useState("");
 
-  }
-  
-  const [count,setcount ]=  useState(0);
+  const [todolist, settodolist] = useState([]);
 
+  const handleChange = (event)=>{
+    setNewTask(event.target.value);
+  };
+
+  const addTask = ()=> {
+    settodolist([...todolist, newTask]);
+  };
+
+  const delete=(taskname)=> {
+    settodolist(todolist.filter()=>{
+      return
+    })
+  };
+
+  return (
+  <div className="App">
+    <div className="addTask">
+      <input onChange={handleChange}/>
+      <button onClick={addTask}> Add Task</button>
+    </div>
+    <div className= "taskList">
+      {todolist.map((task)=>{
+        return <div>
+          <h1>{task}</h1>
+          <button onClick={()=>delete(task)}>X</button>
+          </div>
+      })}
+
+    </div>
+
+  </div>
   
-  const user=[
-    {name:"Mars" ,isGasPlanet: false},
-    {name:"Earth" ,isGasPlanet: false},
-    {name:"Jupiter" ,isGasPlanet:true},
-    {name:"venus" ,isGasPlanet:false},
-    {name:"Neptune" ,isGasPlanet:true},
-    {name:"Uranus" ,isGasPlanet:true}
-  ];
-  
-    return (
-      <div className="App">
-        {user.map((nmme,key)=>!nmme.isGasPlanet && <h1 >{nmme.name} </h1>
-        )}
-        <Greet/>
-        <div>{age} <button onClick={increasedAge}>increase age</button></div>
-        <input type="text" onChange={handleInput}/>{inputvalue}
-        <button 
-        onClick={() => { (setcount(count+1));
-        }}>increase
-        </button>
-        <button 
-        onClick={() => { setcount(count-1);
-        }}>decrease
-        </button>
-        <button 
-        onClick={() => { setcount(0);
-        }}>set to zero
-        </button>
-         <h1> {count}</h1>
-      </div>
-    );
+  );
   
 }
 
